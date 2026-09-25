be9a0917b2df69061e97b0a4f93e60c9e38e064f
Nicolas Nytko <nicknytko@gmail.com>
2026-09-25 15:29:16 -0500

    add bif and ufo tables

--- include/gcc/math.h
+++ include/gcc/math.h
@@ -8,7 +8,12 @@
 
 extern "C"
 {
+    float sqrtf(float x);
+    float sinf(float x);
     float tanf(float x);
+    float asinf(float x);
+    float acosf(float x);
+    float atanf(float x);
     float atan2f(float x, float y);
     float fmodf(float x, float y);
     float cosf(float x);
